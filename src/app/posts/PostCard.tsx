781c85660e2ae60c0f9a670eebbf9d1e959d2151
surajkumar85 <suraj17054209@gmail.com>
2023-03-07 12:09:17 +0530

    added contentlayer
    Not working properly in Next App directory

---
+++ src/app/posts/PostCard.tsx
@@ -0,0 +1,19 @@
+import { format, parseISO } from "date-fns";
+import Link from "next/link";
+
+function PostCard(post: any) {
+  return (
+    <div className="mb-6">
+      <time dateTime={post.date} className="block text-sm text-slate-600">
+        {format(parseISO(post.date), "LLLL d, yyyy")}
+      </time>
+      <h2 className="text-lg">
+        <Link href={post.url}>
+          <a className="text-blue-700 hover:text-blue-900">{post.title}</a>
+        </Link>
+      </h2>
+    </div>
+  );
+}
+
+export default PostCard;

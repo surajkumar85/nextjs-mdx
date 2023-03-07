64623c5bd35d7def2e79d0609d72f4280de23f1b
surajkumar85 <suraj17054209@gmail.com>
2023-03-07 13:24:07 +0530

    added MDX File
    We can read content from mdx files with the help of content layer

--- .contentlayer/.cache/v0.3.0/compiled-contentlayer-config-PR7LGWEH.mjs
+++ .contentlayer/.cache/v0.3.0/compiled-contentlayer-config-PR7LGWEH.mjs
@@ -1,29 +1,33 @@
+// contentlayer.config.js
 import { defineDocumentType, makeSource } from "contentlayer/source-files";
-
-export const Post = defineDocumentType(() => ({
+var Post = defineDocumentType(() => ({
   name: "Post",
   filePathPattern: `**/*.mdx`,
   fields: {
     title: {
       type: "string",
       description: "The title of the post",
-      required: true,
+      required: true
     },
     date: {
       type: "date",
       description: "The date of the post",
-      required: true,
-    },
+      required: true
+    }
   },
   computedFields: {
     url: {
       type: "string",
-      resolve: (post) => `/posts/${post._raw.flattenedPath}`,
-    },
-  },
+      resolve: (post) => `/posts/${post._raw.flattenedPath}`
+    }
+  }
 }));
-
-export default makeSource({
+var contentlayer_config_default = makeSource({
   contentDirPath: "posts",
-  documentTypes: [Post],
+  documentTypes: [Post]
 });
+export {
+  Post,
+  contentlayer_config_default as default
+};
+//# sourceMappingURL=compiled-contentlayer-config-PR7LGWEH.mjs.map
---
+++ src/app/posts/page.tsx
@@ -0,0 +1,42 @@
+import Head from "next/head";
+import Link from "next/link";
+import { compareDesc, format, parseISO } from "date-fns";
+import { allPosts } from "contentlayer/generated";
+
+export async function getAllPost() {
+  const posts = allPosts.sort((a, b) => {
+    return compareDesc(new Date(a.date), new Date(b.date));
+  });
+  return posts;
+}
+
+function PostCard(post) {
+  return (
+    <div className="mb-6">
+      <time dateTime={post.date} className="block text-sm text-slate-600">
+        {format(parseISO(post.date), "LLLL d, yyyy")}
+      </time>
+      <h2 className="text-lg">
+        <Link href={post.url}>{post.title}</Link>
+      </h2>
+    </div>
+  );
+}
+
+export default async function Home() {
+  const posts = await getAllPost();
+
+  return (
+    <div className="mx-auto max-w-2xl py-16 text-center">
+      <Head>
+        <title>Contentlayer Blog Example</title>
+      </Head>
+
+      <h1 className="mb-8 text-3xl font-bold">Contentlayer Blog Example</h1>
+
+      {posts.map((post, idx) => (
+        <PostCard key={idx} {...post} />
+      ))}
+    </div>
+  );
+}
